91ff4485dc210b8735e603ef962ab88b64f6c159
Bipin M <bipin.mandoti@succevo.com>
2021-04-04 12:55:43 +0530

    setup templates

--- resources/views/layout/template.blade.php
+++ resources/views/layout/template.blade.php
@@ -42,8 +42,31 @@
 
  <div class="app-content content">
    <div class="content-wrapper">
-      <div class="content-header row">
-      </div>
+   <div class="content-header row">
+          <div class="content-header-left col-md-8 col-12 mb-2 breadcrumb-new">
+            <h3 class="content-header-title mb-0 d-inline-block">Basic Forms</h3>
+            <div class="row breadcrumbs-top d-inline-block">
+              <div class="breadcrumb-wrapper col-12">
+                <ol class="breadcrumb">
+                  <li class="breadcrumb-item"><a href="index-2.html">Home</a>
+                  </li>
+                  <li class="breadcrumb-item"><a href="#">Form Layouts</a>
+                  </li>
+                  <li class="breadcrumb-item active">Basic Forms
+                  </li>
+                </ol>
+              </div>
+            </div>
+          </div>
+          <div class="content-header-right col-md-4 col-12">
+            <div class="btn-group float-md-right">
+              <button class="btn btn-info dropdown-toggle" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="icon-settings mr-1"></i>Action</button>
+              <div class="dropdown-menu arrow"><a class="dropdown-item" href="#"><i class="fa fa-calendar mr-1"></i> Calender</a><a class="dropdown-item" href="#"><i class="fa fa-cart-plus mr-1"></i> Cart</a><a class="dropdown-item" href="#"><i class="fa fa-life-ring mr-1"></i> Support</a>
+                <div class="dropdown-divider"></div><a class="dropdown-item" href="#"><i class="fa fa-cog mr-1"></i> Settings</a>
+              </div>
+            </div>
+          </div>
+        </div>
       <div class="content-body">
         @yield('content')
       </div>
@@ -65,6 +88,9 @@
     <script src="{{ asset("app-assets/js/core/app-menu.min.js")}}"></script>
     <script src="{{ asset("app-assets/js/core/app.min.js")}}"></script>
     <script src="{{ asset("app-assets/js/scripts/customizer.min.js")}}"></script>
-    <!-- END ROBUST JS-->     
+    <!-- END ROBUST JS-->
+
+    @yield('javascript')
+
   </body>
 </html>
---
+++ resources/views/login/index.blade.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html class="loading" lang="en" data-textdirection="ltr">
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui">
+    <meta name="description" content="Robust admin is super flexible, powerful, clean &amp; modern responsive bootstrap 4 admin template.">
+    <meta name="keywords" content="admin template, robust admin template, dashboard template, flat admin template, responsive admin template, web app, crypto dashboard, bitcoin dashboard">
+    <meta name="author" content="PIXINVENT">
+    <title>Login</title>
+    <link rel="apple-touch-icon" href="app-assets/images/ico/apple-icon-120.png">
+    <link rel="shortcut icon" type="image/x-icon" href="https://pixinvent.com/bootstrap-admin-template/robust/app-assets/images/ico/favicon.ico">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i%7CMuli:300,400,500,700" rel="stylesheet">
+    <!-- BEGIN VENDOR CSS-->
+    <link rel="stylesheet" type="text/css" href="{{asset("app-assets/css/vendors.min.css")}}">    
+    <!-- END VENDOR CSS-->
+    <!-- BEGIN ROBUST CSS-->
+    <link rel="stylesheet" type="text/css" href="{{asset("app-assets/css/app.min.css")}}">
+    <!-- END ROBUST CSS-->
+    <!-- BEGIN Page Level CSS-->
+    <link rel="stylesheet" type="text/css" href="{{asset("app-assets/css/core/menu/menu-types/vertical-menu.min.css")}}">
+    <link rel="stylesheet" type="text/css" href="{{asset("app-assets/css/core/colors/palette-gradient.min.css")}}">
+    <link rel="stylesheet" type="text/css" href="{{asset("app-assets/css/pages/login-register.min.css")}}">
+    <!-- END Page Level CSS-->
+    <!-- BEGIN Custom CSS-->
+    <link rel="stylesheet" type="text/css" href="{{asset("assets/css/style.css")}}">
+    <!-- END Custom CSS-->
+  </head>
+  <body class="vertical-layout vertical-menu 1-column   menu-expanded blank-page blank-page" data-open="click" data-menu="vertical-menu" data-col="1-column">
+    <!-- ////////////////////////////////////////////////////////////////////////////-->
+    <div class="app-content content">
+      <div class="content-wrapper">
+        <div class="content-header row">
+        </div>
+        <div class="content-body"><section class="flexbox-container">
+    <div class="col-12 d-flex align-items-center justify-content-center">
+        <div class="col-md-4 col-10 box-shadow-2 p-0">
+            <div class="card border-grey border-lighten-3 m-0">
+                <div class="card-header border-0">
+                    <div class="card-title text-center">
+                        <div class="p-1"><img src="app-assets/images/logo/logo-dark.png" alt="branding logo"></div>
+                    </div>
+                    <h6 class="card-subtitle line-on-side text-muted text-center font-small-3 pt-2"><span>Login</span></h6>
+                </div>
+                <div class="card-content">
+                    <div class="card-body">
+                        <form class="form-horizontal form-simple" action="" novalidate>
+                            <fieldset class="form-group position-relative has-icon-left mb-0">
+                                <input type="text" class="form-control form-control-lg input-lg" id="user-name" placeholder="Username" required>
+                                <div class="form-control-position">
+                                    <i class="ft-user"></i>
+                                </div>
+                            </fieldset>
+                            <fieldset class="form-group position-relative has-icon-left">
+                                <input type="password" class="form-control form-control-lg input-lg" id="user-password" placeholder="Password" required>
+                                <div class="form-control-position">
+                                    <i class="fa fa-key"></i>
+                                </div>
+                            </fieldset>                            
+                            <button type="submit" class="btn btn-info btn-lg btn-block"><i class="ft-unlock"></i> Login</button>
+                        </form>
+                    </div>
+                </div>              
+            </div>
+        </div>
+    </div>
+</section>
+
+        </div>
+      </div>
+    </div>
+    <!-- ////////////////////////////////////////////////////////////////////////////-->
+
+    <!-- BEGIN VENDOR JS-->
+    <script src="{{asset("app-assets/vendors/js/vendors.min.js")}}"></script>
+    <!-- BEGIN VENDOR JS-->
+    <!-- BEGIN PAGE VENDOR JS-->    
+    <script src="{{asset("app-assets/vendors/js/forms/validation/jqBootstrapValidation.js")}}"></script>
+    <!-- END PAGE VENDOR JS-->
+    <!-- BEGIN ROBUST JS-->
+    <script src="{{asset("app-assets/js/core/app-menu.min.js")}}"></script>
+    <script src="{{asset("app-assets/js/core/app.min.js")}}"></script>
+    <!-- END ROBUST JS-->
+    <!-- BEGIN PAGE LEVEL JS-->
+    <script src="{{asset("app-assets/js/scripts/forms/form-login-register.min.js")}}"></script>
+    <!-- END PAGE LEVEL JS-->
+  </body>
+
+</html>
